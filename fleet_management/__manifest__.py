11b3da5cfa380fcfbab90eb868d4bafa4a9d1119
TysonPine <kaungthawmaung91@gmail.com>
2025-11-23 00:20:06 +0700

    Cron job added where noti is shown through chatter box as of the initial push

--- fleet_management/__manifest__.py
+++ fleet_management/__manifest__.py
@@ -4,8 +4,9 @@
     'summary': 'Manage vehicles, driver, fuel logs and maintenance schedule',
     'category': 'Operations/Fleet',
     'author': 'Tyson R. Pine',
-    'depends': ['base'],
+    'depends': ['base', 'mail'],
     'data': [
+        'views/fleet_cron_view.xml',
         'views/fleet_management_view.xml',
         'views/fleet_fuel_log_view.xml',
         'views/fleet_maintenance_log_view.xml',
--- fleet_management/models/fleet_maintenance_log.py
+++ fleet_management/models/fleet_maintenance_log.py
@@ -1,8 +1,11 @@
 from odoo import models, fields, api
+import logging
+_logger = logging.getLogger(__name__)
 
 class FleetMaintenanceLog(models.Model):
     _name = 'fleet.maintenance.log'
     _description = 'Fleet Maintenance Log'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
     vehicle_id = fields.Many2one('fleet.management', string="Vehicle", required=True)
     maintenance_type = fields.Selection([
@@ -20,3 +23,20 @@ class FleetMaintenanceLog(models.Model):
         ('completed', 'Completed'),
         ('canceled', 'Canceled')
     ], string="Status", default='scheduled')
+
+
+    def _cron_check_maintenance_due(self):
+        today = fields.Date.today()
+        logs = self.search([
+            ('next_due_date', '!=', False),
+            ('next_due_date', '<=', today),
+            ('state', '=', 'scheduled')
+        ])
+        
+        for rec in logs:
+            rec.message_post(
+                body=f"Maintenance is due for vehicle {rec.vehicle_id.name} on {rec.next_due_date}.",
+                subject="Maintenance Due Reminder",
+                message_type="notification",       
+                subtype_xmlid="mail.mt_comment",  
+        )
